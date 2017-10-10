64e1fbcde76efa01a187732c976bfaee6b56965f
aredondasm <aredondasm@a6c684d4-4dd2-4c32-a4e8-b2b4837a8c5a>
2017-10-10 10:19:22 +0000

    bts paper

--- read_bts.m
+++ read_bts.m
@@ -1,13 +1,14 @@
 fpath='data_set_1/bts'
-l=dir(fullfile(fpath,'*.dat'))
+l=dir(fullfile(fpath,'TOC_BTS*.txt'))
 dobson=[];
 for i=1:length(l)
-   dob=textread(fullfile(fpath,l(i).name),'','headerlines',2,'emptyvalue',NaN);
-   fecha=datenum(dob(:,1:6));
-   dobson=[dobson;[fecha,dob(:,7:end)]];
+   %dob=textread(fullfile(fpath,l(i).name),'%2d.%2d.%4d %2d:%2d:%4d %d','headerlines',3,'emptyvalue',NaN,'delimiter',' ');
+   [d,m,a,hh,mm,ss,o3]=textread(fullfile(fpath,l(i).name),'%2d.%2d.%4d %2d:%2d:%4d %d','headerlines',3,'emptyvalue',NaN,'delimiter',' ');
+   fecha=datenum(a,m,d,hh,mm,ss);
+   dobson=[dobson;[fecha,o3]];
 end
 bts=dobson;   
-leg='Date O3 O3_std';
+leg='Date O3 ';
 leg=mmcellstr(strrep(leg,' ','|'));
 
 BTS_orig=array2table(dobson,'VariableNames' ,leg,'RowNames',cellstr(datestr(dobson(:,1))));
@@ -17,12 +18,15 @@ writetable(BTS_orig,'Atmoz_o3_set1.xls','Sheet','BTS','WriteRowNames',true);
 
 %%
 t_dep=BTS_orig;
-
 t_dep.O3=t_dep.O3;
-t_dep.O3_STD=t_dep.O3_std;
+t_dep.O3_STD=NaN*t_dep.O3;
 t_dep.AIRM=NaN*t_dep.O3;
 t_dep.Time=t_dep.date;
 %t_dep.Date=Date;
 t_dep=t_dep(:,{'Time','Date','O3','O3_STD','AIRM'});
 
-t_set_1{n_inst}=t_dep(:,{'Time','Date','O3','O3_STD','AIRM'});
+try
+    t_set_1{n_inst}=t_dep(:,{'Time','Date','O3','O3_STD','AIRM'});
+catch
+    disp('no set 1');
+end
